2e47fd301f7f2eb52d11eedbfd839521116c6b69
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-06-14 02:41:41 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -51,7 +51,7 @@ if age > 0:
         # For each vaccine the user has taken, check if they need any more doses
         for vaccine in vaccine_selection:
             vaccine_key = vaccine.strip()
-            show_completion = st.checkbox(f"Do you want to check if you have completed the series for {vaccine_key}?")
+            show_completion = st.checkbox(f"Check the box if you want to check if you have completed the series for {vaccine_key}")
             if show_completion:
                 doses_taken = st.number_input(f"How many doses of {vaccine_key} have you taken?", min_value=0, value=0)
                 if doses_taken > 0:
