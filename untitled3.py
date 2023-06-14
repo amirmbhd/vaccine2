1c06bfcb1d3e46c5d0374fbb14690928238695c5
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-06-14 02:20:32 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -44,35 +44,41 @@ if not restart_clicked:
     age_month = st.selectbox("Select your age (months):", months_options)
     age_year = st.selectbox("Select your age (years):", years_options)
 
-    # Determine which vaccines the user is eligible for
-    age = age_month + age_year * 12
-    eligible_vaccines = {k: v for k, v in vaccines.items() if age in v["ages"]}
+    # Start button
+    start = st.button("Start")
 
-    # If the user is not eligible for any vaccines, print a message and exit
-    if not eligible_vaccines:
-        st.write("You are not currently eligible for any vaccines.")
-    else:
-        # Otherwise, print the eligible vaccines
-        st.write("You are eligible for the following vaccines:")
-        for vaccine, info in eligible_vaccines.items():
-            st.write(f"{vaccine}: {info['doses']} doses")
+    if start:
+        # Calculate the age in months
+        age = age_month + age_year * 12
 
-        # Ask the user which vaccines they have already taken
-        vaccine_selection = st.multiselect("Select the vaccines you have already taken:", list(eligible_vaccines.keys()))
+        # Determine which vaccines the user is eligible for
+        eligible_vaccines = {k: v for k, v in vaccines.items() if age in v["ages"]}
 
-        # For each vaccine the user has taken, check if they need any more doses
-        for vaccine in vaccine_selection:
-            vaccine_key = vaccine.strip()
-            show_completion = st.checkbox(f"Do you want to check if you have completed the series for {vaccine_key}?")
-            if show_completion:
-                doses_taken = st.number_input(f"How many doses of {vaccine_key} have you taken?", min_value=0, value=0)
-                doses_needed = eligible_vaccines[vaccine_key]["doses"] - doses_taken
-                if doses_needed > 0:
-                    st.write(f"You need {doses_needed} more doses of {vaccine_key}.")
-                else:
-                    st.write(f"You have completed the required doses for {vaccine_key}.")
+        # If the user is not eligible for any vaccines, print a message and exit
+        if not eligible_vaccines:
+            st.write("You are not currently eligible for any vaccines.")
+        else:
+            # Otherwise, print the eligible vaccines
+            st.write("You are eligible for the following vaccines:")
+            for vaccine, info in eligible_vaccines.items():
+                st.write(f"{vaccine}: {info['doses']} doses")
 
-    # Restart button
-    st.button("Restart", on_click=reset_program)
+            # Ask the user which vaccines they have already taken
+            vaccine_selection = st.multiselect("Select the vaccines you have already taken:", list(eligible_vaccines.keys()))
+
+            # For each vaccine the user has taken, check if they need any more doses
+            for vaccine in vaccine_selection:
+                vaccine_key = vaccine.strip()
+                show_completion = st.checkbox(f"Do you want to check if you have completed the series for {vaccine_key}?")
+                if show_completion:
+                    doses_taken = st.number_input(f"How many doses of {vaccine_key} have you taken?", min_value=0, value=0)
+                    doses_needed = eligible_vaccines[vaccine_key]["doses"] - doses_taken
+                    if doses_needed > 0:
+                        st.write(f"You need {doses_needed} more doses of {vaccine_key}.")
+                    else:
+                        st.write(f"You have completed the required doses for {vaccine_key}.")
+
+        # Restart button
+        st.button("Restart", on_click=reset_program)
 else:
     st.experimental_rerun()
