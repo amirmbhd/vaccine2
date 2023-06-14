a913d38c7b1c114265e34557d6dc6d9d06996fa6
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-06-14 01:51:54 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -21,41 +21,51 @@ for _, row in vaccine_df.iterrows():
 months_options = list(range(13))  # 0 to 12
 years_options = list(range(19))  # 0 to 18
 
+# Define the possible states of the application
+STATE_WELCOME = "welcome"
+STATE_PROGRAM = "program"
+
+# Initialize the state
+state = st.session_state.get("state", STATE_WELCOME)
+
 # Welcome message and program description
-st.title("Vaccine Recommendation Program")
-st.write("Welcome to the Vaccine Recommendation Program! This program will tell you which vaccines you are eligible for based on your age. You can also enter which vaccines you have already taken, and the program will tell you if you need any more doses.")
+if state == STATE_WELCOME:
+    st.title("Vaccine Recommendation Program")
+    st.write("Welcome to the Vaccine Recommendation Program! This program will tell you which vaccines you are eligible for based on your age. You can also enter which vaccines you have already taken, and the program will tell you if you need any more doses.")
+    if st.button('Start'):
+        state = STATE_PROGRAM
 
-# Ask the user for their age
-age_month = st.selectbox("Select your age (months):", months_options)
-age_year = st.selectbox("Select your age (years):", years_options)
+# Handle the state transitions
+if state == STATE_PROGRAM:
+    # Ask the user for their age
+    age_month = st.selectbox("Select your age (months):", months_options)
+    age_year = st.selectbox("Select your age (years):", years_options)
 
-# Calculate the age in months
-age = age_month + age_year * 12
+    # Calculate the age in months
+    age = age_month + age_year * 12
 
-# Determine which vaccines the user is eligible for
-eligible_vaccines = {k: v for k, v in vaccines.items() if age in v["ages"]}
+    # Determine which vaccines the user is eligible for
+    eligible_vaccines = {k: v for k, v in vaccines.items() if age in v["ages"]}
 
-# If the user is not eligible for any vaccines, print a message and exit
-if not eligible_vaccines:
-    st.write("You are not currently eligible for any vaccines.")
-else:
-    # Otherwise, print the eligible vaccines
-    st.write("You are eligible for the following vaccines:")
-    vaccine_options = [f"{k}: {v['doses']} doses" for k, v in eligible_vaccines.items()]
-    for i, option in enumerate(vaccine_options, start=1):
-        st.write(f"{i}. {option}")
+    # If the user is not eligible for any vaccines, print a message and exit
+    if not eligible_vaccines:
+        st.write("You are not currently eligible for any vaccines.")
+    else:
+        # Otherwise, print the eligible vaccines
+        st.write("You are eligible for the following vaccines:")
+        vaccine_options = [f"{k}: {v['doses']} doses" for k, v in eligible_vaccines.items()]
+        for i, option in enumerate(vaccine_options, start=1):
+            st.write(f"{i}. {option}")
 
-    # Ask the user which vaccines they have already taken
-    vaccine_selection = st.multiselect("Select the vaccines you have already taken:", vaccine_options)
+        # Ask the user which vaccines they have already taken
+        vaccine_selection = st.multiselect("Select the vaccines you have already taken:", vaccine_options)
 
-    # For each vaccine the user has taken, check if they need any more doses
-    for vaccine in vaccine_selection:
-        vaccine_key = vaccine.split(":")[0].strip()
-        show_completion = st.checkbox(f"Do you want to check if you have completed the series for {vaccine_key}?")
-        if show_completion:
-            doses_taken = st.number_input(f"How many doses of {vaccine_key} have you taken?", min_value=0, value=0)
-            doses_needed = eligible_vaccines[vaccine_key]["doses"] - doses_taken
-            if doses_needed > 0:
-                st.write(f"You need {doses_needed} more doses of {vaccine_key}.")
-            else:
-                st.write(f"You have completed the required doses for {vaccine_key}.")
+        # For each vaccine the user has taken, check if they need any more doses
+        for vaccine in vaccine_selection:
+            vaccine_key = vaccine.split(":")[0].strip()
+            show_completion = st.checkbox(f"Do you want to check if you have completed the series for {vaccine_key}?")
+            if show_completion:
+                doses_taken = st.number_input(f"How many doses of {vaccine_key} have you taken?", min_value=0, value=0)
+                doses_needed = eligible_vaccines[vaccine_key]["doses"] - doses_taken
+                if doses_needed > 0:
+                    st
