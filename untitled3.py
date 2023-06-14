3c51cc461955a4aea3cc45ec993e3adf81717b4d
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-06-14 01:01:43 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -8,53 +8,42 @@ vaccines = {
     "Influenza": {"ages": range(6, 200), "doses": 1, "name": "Influenza"}
 }
 
-# Define the possible states of the application
-STATE_WELCOME = "welcome"
-STATE_PROGRAM = "program"
-
-# Initialize the state
-state = st.session_state.get("state", STATE_WELCOME)
+# Define the months and years options
+months_options = list(range(13))  # 0 to 12
+years_options = list(range(19))  # 0 to 18
 
 # Welcome message and program description
 st.title("Vaccine Recommendation Program")
 st.write("Welcome to the Vaccine Recommendation Program! This program will tell you which vaccines you are eligible for based on your age. You can also enter which vaccines you have already taken, and the program will tell you if you need any more doses.")
 
-# Handle the state transitions
-if state == STATE_WELCOME:
-    if st.button('Start'):
-        state = STATE_PROGRAM
-elif state == STATE_PROGRAM:
-    # Ask the user for their age
-    age = st.number_input("Please enter your age in yers:", min_value=0, value=0)
+# Ask the user for their age
+age_month = st.selectbox("Select your age (months):", months_options)
+age_year = st.selectbox("Select your age (years):", years_options)
 
-    # Determine which vaccines the user is eligible for
-    eligible_vaccines = {k: v for k, v in vaccines.items() if age in v["ages"]}
+# Calculate the age in months
+age = age_month + age_year * 12
 
-    # If the user is not eligible for any vaccines, print a message and exit
-    if not eligible_vaccines:
-        st.write("You are not currently eligible for any vaccines.")
-    else:
-        # Otherwise, print the eligible vaccines
-        st.write("You are eligible for the following vaccines:")
-        vaccine_options = [f"{k}: {v['name']}" for k, v in eligible_vaccines.items()]
-        for i, option in enumerate(vaccine_options, start=1):
-            st.write(f"{i}. {option}")
+# Determine which vaccines the user is eligible for
+eligible_vaccines = {k: v for k, v in vaccines.items() if age in v["ages"]}
 
-        # Ask the user which vaccines they have already taken
-        vaccine_selection = st.multiselect("Select the vaccines you have already taken:", vaccine_options)
+# If the user is not eligible for any vaccines, print a message and exit
+if not eligible_vaccines:
+    st.write("You are not currently eligible for any vaccines.")
+else:
+    # Otherwise, print the eligible vaccines
+    st.write("You are eligible for the following vaccines:")
+    vaccine_options = [f"{k}: {v['name']}" for k, v in eligible_vaccines.items()]
+    for i, option in enumerate(vaccine_options, start=1):
+        st.write(f"{i}. {option}")
 
-        # For each vaccine the user has taken, check if they need any more doses
-        for vaccine in vaccine_selection:
-            vaccine_key = vaccine.split(":")[0].strip()
-            doses_taken = st.number_input(f"How many doses of {vaccine_key} have you taken?", min_value=0, value=0)
-            if doses_taken < eligible_vaccines[vaccine_key]["doses"]:
-                st.write(f"You need {eligible_vaccines[vaccine_key]['doses'] - doses_taken} more doses of {vaccine_key}.")
-            else:
-                st.write(f"You have completed the required doses for {vaccine_key}.")
+    # Ask the user which vaccines they have already taken
+    vaccine_selection = st.multiselect("Select the vaccines you have already taken:", vaccine_options)
 
-        # Add a button to restart the application
-        if st.button('Restart'):
-            state = STATE_WELCOME
-
-# Update the state in session state
-st.session_state["state"] = state
+    # For each vaccine the user has taken, check if they need any more doses
+    for vaccine in vaccine_selection:
+        vaccine_key = vaccine.split(":")[0].strip()
+        doses_taken = st.number_input(f"How many doses of {vaccine_key} have you taken?", min_value=0, value=0)
+        if doses_taken < eligible_vaccines[vaccine_key]["doses"]:
+            st.write(f"You need {eligible_vaccines[vaccine_key]['doses'] - doses_taken} more doses of {vaccine_key}.")
+        else:
+            st.write(f"You have completed the required doses for {vaccine_key}.")
