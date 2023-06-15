8be12d2a553575b0b9942a92b84cd68b7952fc83
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-06-15 07:15:36 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -79,74 +79,57 @@ if age > 0:
         "", list(eligible_vaccines.keys()) + ["None"]
     )
 
-    # Create a form that requires the user to submit
-    with st.sidebar.form(key="my_form"):
-        submit_button = st.form_submit_button(label="Generate Vaccine Recommendation")
+    if vaccine_selection and "None" not in vaccine_selection:
+        vaccines_not_taken = [
+            vaccine for vaccine in eligible_vaccines.keys() if vaccine not in vaccine_selection
+        ]
 
-    if submit_button:
-        if vaccine_selection and "None" not in vaccine_selection:
-            vaccines_not_taken = [
-                vaccine for vaccine in eligible_vaccines.keys() if vaccine not in vaccine_selection
-            ]
+        # Define the data for the table
+        data = []
+        for vaccine, info in eligible_vaccines.items():
+            status = "Completed" if vaccine in vaccine_selection else "Pending"
+            data.append([vaccine, info["doses"], status])
 
-            # Define the data for the table
-            data = []
-            for vaccine, info in eligible_vaccines.items():
-                status = "Completed" if vaccine in vaccine_selection else "Pending"
-                data.append([vaccine, info["doses"], status])
+        # Create the DataFrame
+        df = pd.DataFrame(data, columns=["Vaccine Name", "Total Doses", "Status"])
+        df = df.sort_values(by="Status", ascending=False)
 
-            # Create the DataFrame
-            df = pd.DataFrame(data, columns=["Vaccine Name", "Total Doses", "Status"])
-            df = df.sort_values(by="Status", ascending=False)
+        # Display the table
+        st.table(df.style.set_properties(**{"text-align": "center"}).\
+            set_table_styles([{'selector': 'th', 'props': [('font-weight', 'bold')]}]))
 
-            # Display the table
-            st.table(df.style.set_properties(**{"text-align": "center"}).\
-                set_table_styles([{'selector': 'th', 'props': [('font-weight', 'bold')]}]))
-
+        st.markdown(
+            "**<span style='color:#708090'>The timeline for your remaining vaccines:</span>**",
+            unsafe_allow_html=True,
+        )
+        for vaccine in vaccines_not_taken:
             st.markdown(
-                "**<span style='color:#708090'>The timeline for your remaining vaccines:</span>**",
-                unsafe_allow_html=True,
+                f"**<span style='color:#708090'>{vaccine}:</span>**", unsafe_allow_html=True
             )
-            for vaccine in vaccines_not_taken:
+            for dose, time in eligible_vaccines[vaccine]["timeline"].items():
+                st.write(f"{dose}: {time}")
+            if vaccine.startswith("Meningococcal:") and meningococcal_note:
                 st.markdown(
-                    f"**<span style='color:#708090'>{vaccine}:</span>**", unsafe_allow_html=True
+                    "<span style='color:#708090'>(Note: You are eligible for multiple types of Meningococcal vaccines. The timeline displayed is specific to the type closest to your age, but you may be eligible for others with different schedules.)</span>",
+                    unsafe_allow_html=True,
                 )
-                for dose, time in eligible_vaccines[vaccine]["timeline"].items():
-                    st.write(f"{dose}: {time}")
-                if vaccine.startswith("Meningococcal:") and meningococcal_note:
-                    st.markdown(
-                        "<span style='color:#708090'>(Note: You are eligible for multiple types of Meningococcal vaccines. The timeline displayed is specific to the type closest to your age, but you may be eligible for others with different schedules.)</span>",
-                        unsafe_allow_html=True,
-                    )
 
-            st.markdown("**Check vaccine series completion:**", unsafe_allow_html=True)
+        st.markdown("**Check vaccine series completion:**", unsafe_allow_html=True)
+        for vaccine in vaccine_selection:
+            vaccine_key = vaccine.strip()
+            doses_taken = st.number_input(
+                f"How many doses of {vaccine_key} have you taken?",
+                min_value=0,
+                value=0,
+            )
+            if doses_taken > 0:
+                doses_needed = vaccines[vaccine_key]["doses"] - doses_taken  # Use 'vaccines' instead of 'eligible_vaccines'
+                if doses_needed > 0:
+                    st.write(f"You need {doses_needed} more doses of {vaccine_key}.")
+                else:
+                    st.write(f"You have completed the required doses for {vaccine_key}.")
 
-            # Generate checkboxes and number inputs for each vaccine at once
-            check_completion_dict = {}
-            doses_taken_dict = {}
-            for vaccine in vaccine_selection:
-                vaccine_key = vaccine.strip()
-                check_completion_dict[vaccine_key] = st.checkbox(
-                    f"Do you want to check if you have completed the series for {vaccine_key}?"
-                )
-                if check_completion_dict[vaccine_key]:
-                    doses_taken_dict[vaccine_key] = st.number_input(
-                        f"How many doses of {vaccine_key} have you taken?",
-                        min_value=0,
-                        value=0,
-                    )
-
-            # Now that we have the inputs, calculate the doses needed for each vaccine
-            for vaccine in vaccine_selection:
-                vaccine_key = vaccine.strip()
-                if check_completion_dict[vaccine_key] and doses_taken_dict[vaccine_key] > 0:
-                    doses_needed = vaccines[vaccine_key]["doses"] - doses_taken_dict[vaccine_key]
-                    if doses_needed > 0:
-                        st.write(f"You need {doses_needed} more doses of {vaccine_key}.")
-                    else:
-                        st.write(f"You have completed the series for {vaccine_key}.")
-
-        else:
-            st.write("You did not select any vaccines. Please try again.")
+    else:
+        st.write("You did not select any vaccines. Please try again.")
 else:
     st.markdown("**You have to enter your age!**")
