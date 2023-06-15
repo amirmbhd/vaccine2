3c9a5451198e2950c546d66290c922146d6ae365
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-06-15 03:23:32 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -55,39 +55,34 @@ if age > 0:
         eligible_vaccines[f"Meningococcal: {closest_vaccine}"] = vaccines[closest_vaccine]
         meningococcal_note = True
 
-    if eligible_vaccines:
-        st.sidebar.markdown("**<span style='color:black'>Please select the vaccines you have already taken (You can select multiple):</span>**", unsafe_allow_html=True)
-        vaccine_selection = st.sidebar.multiselect("", list(eligible_vaccines.keys()) + ["None"])
-        vaccines_not_taken = [vaccine for vaccine in eligible_vaccines.keys() if vaccine not in vaccine_selection]
+    # Sidebar for already taken vaccines
+    st.sidebar.markdown("**<span style='color:black'>Please select the vaccines you have already taken (You can select multiple):</span>**", unsafe_allow_html=True)
+    vaccine_selection = st.sidebar.multiselect("", list(eligible_vaccines.keys()) + ["None"])
 
-        if vaccine_selection and "None" not in vaccine_selection and vaccines_not_taken:
-            if st.sidebar.button("Generate Vaccine Recommendation"):
-                st.markdown("**<span style='color:#708090'>You are eligible for the following vaccines:</span>**", unsafe_allow_html=True)
-                
-                # Create a table for eligible vaccines
-                table_data = []
-                for vaccine, info in eligible_vaccines.items():
-                    table_data.append([vaccine, info['doses'], 'Pending'])
-                table_data = sorted(table_data, key=lambda x: x[2])
-                table_data.insert(0, ['Vaccine Name', 'Total Doses', 'Status'])
-                
-                st.table(table_data)
-                
-                st.markdown("**<span style='color:#708090'>The timeline for your remaining vaccines:</span>**", unsafe_allow_html=True)
-                for vaccine in vaccines_not_taken:
-                    st.markdown(f"**<span style='color:#708090'>{vaccine}:</span>**", unsafe_allow_html=True)
-                    for dose, time in eligible_vaccines[vaccine]["timeline"].items():
-                        st.write(f"{dose}: {time}")
-                    if vaccine.startswith("Meningococcal:") and meningococcal_note:
-                        st.markdown("<span style='color:#708090'>(Note: You are eligible for multiple types of Meningococcal vaccines. The timeline displayed is specific to the type closest to your age, but you may be eligible for others with different schedules.)</span>", unsafe_allow_html=True)
-                        
+    # Create a form that requires the user to submit
+    with st.sidebar.form(key='my_form'):
+        submit_button = st.form_submit_button(label='Generate Vaccine Recommendation')
+
+    if submit_button:
+        if vaccine_selection and "None" not in vaccine_selection:
+            vaccines_not_taken = [vaccine for vaccine in eligible_vaccines.keys() if vaccine not in vaccine_selection]
+
+            st.markdown("**<span style='color:#708090'>You are eligible for the following vaccines:</span>**", unsafe_allow_html=True)
+            for vaccine, info in eligible_vaccines.items():
+                st.write(f"{vaccine}: {info['doses']} doses")
+
+            st.markdown("**<span style='color:#708090'>The timeline for your remaining vaccines:</span>**", unsafe_allow_html=True)
+            for vaccine in vaccines_not_taken:
+                st.markdown(f"**<span style='color:#708090'>{vaccine}:</span>**", unsafe_allow_html=True)
+                for dose, time in eligible_vaccines[vaccine]["timeline"].items():
+                    st.write(f"{dose}: {time}")
+                if vaccine.startswith("Meningococcal:") and meningococcal_note:
+                    st.markdown("<span style='color:#708090'>(Note: You are eligible for multiple types of Meningococcal vaccines. The timeline displayed is specific to the type closest to your age, but you may be eligible for others with different schedules.)</span>", unsafe_allow_html=True)
+
+            st.markdown("**Check vaccine series completion:**", unsafe_allow_html=True)
             for vaccine in vaccine_selection:
                 vaccine_key = vaccine.strip()
-                default_value = "No"
-                show_completion_key = f"show_completion_{vaccine_key}"
-                if show_completion_key not in st.session_state:
-                    st.session_state[show_completion_key] = default_value
-                show_completion = st.radio(f"Do you want to check if you have completed the series for {vaccine_key}?", ["Yes", "No"], key=show_completion_key)
+                show_completion = st.radio(f"Do you want to check if you have completed the series for {vaccine_key}?", ["Yes", "No"])
                 if show_completion == "Yes":
                     doses_taken = st.number_input(f"How many doses of {vaccine_key} have you taken?", min_value=0, value=0)
                     if doses_taken > 0:
