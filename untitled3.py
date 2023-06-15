188804a22f2f770e2f793ad6313bf3b8351fbcc5
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-06-15 03:55:03 -0400

    Update untitled3.py
    + bart

--- untitled3.py
+++ untitled3.py
@@ -88,17 +88,23 @@ if age > 0:
                 if vaccine.startswith("Meningococcal:") and meningococcal_note:
                     st.markdown("<span style='color:#708090'>(Note: You are eligible for multiple types of Meningococcal vaccines. The timeline displayed is specific to the type closest to your age, but you may be eligible for others with different schedules.)</span>", unsafe_allow_html=True)
 
-            st.markdown("**Check vaccine series completion:**", unsafe_allow_html=True)
+            def check_series_completion(vaccine_key, doses_taken):
+                doses_needed = eligible_vaccines[vaccine_key]["doses"] - doses_taken
+                if doses_needed > 0:
+                    return False
+                else:
+                    return True
+
             for vaccine in vaccine_selection:
                 vaccine_key = vaccine.strip()
                 show_completion = st.radio(f"Do you want to check if you have completed the series for {vaccine_key}?", ["No", "Yes"], index=0)
                 if show_completion == "Yes":
                     doses_taken = st.number_input(f"How many doses of {vaccine_key} have you taken?", min_value=0, value=0)
                     if doses_taken > 0:
-                        doses_needed = vaccines[vaccine_key]["doses"] - doses_taken  # Use 'vaccines' instead of 'eligible_vaccines'
-                        if doses_needed > 0:
-                            st.write(f"You need {doses_needed} more doses of {vaccine_key}.")
-                        else:
+                        if check_series_completion(vaccine_key, doses_taken):
                             st.write(f"You have completed the required doses for {vaccine_key}.")
+                        else:
+                            st.write(f"You need {doses_needed} more doses of {vaccine_key}.")
+
 else:
     st.write(" ")
