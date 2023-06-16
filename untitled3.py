74e40cc516895a8cb2fc6f7d2f98107ba0830007
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-06-15 20:30:38 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -70,53 +70,60 @@ if age > 0:
         eligible_vaccines[f"Meningococcal: {closest_vaccine}"] = vaccines[closest_vaccine]
         meningococcal_note = True
 
+    # Sidebar for already taken vaccines
     st.sidebar.markdown(
-        "**Please select the vaccines you have already taken (You can select multiple):**"
+        "**<span style='color:black'>Please select the vaccines you have already taken (You can select multiple):</span>**",
+        unsafe_allow_html=True,
     )
     vaccine_selection = st.sidebar.multiselect(
         "", list(eligible_vaccines.keys()) + ["None"]
     )
 
-    if vaccine_selection and "None" not in vaccine_selection:
-        vaccines_not_taken = [
-            vaccine for vaccine in eligible_vaccines.keys() if vaccine not in vaccine_selection
-        ]
+    # Define the data for the table
+    data = []
+    for vaccine, info in eligible_vaccines.items():
+        status = "Completed" if vaccine in vaccine_selection else "Pending"
+        data.append([vaccine, info["doses"], status])
 
-        # Define the data for the table
-        data = []
-        for vaccine, info in eligible_vaccines.items():
-            status = "Completed" if vaccine in vaccine_selection else "Pending"
-            data.append([vaccine, info["doses"], status])
+    # Create the DataFrame
+    df = pd.DataFrame(data, columns=["Vaccine Name", "Total Doses", "Status"])
+    df = df.sort_values(by="Status", ascending=False)
 
-        # Create the DataFrame
-        df = pd.DataFrame(data, columns=["Vaccine Name", "Total Doses", "Status"])
-        df = df.sort_values(by="Status", ascending=False)
+    # Display the table
+    df_styled = df.style.hide().set_properties(**{"text-align": "center"}).apply(color_rows, axis=1).\
+                set_table_styles([{'selector': 'th', 'props': [('text-align', 'center'), ('font-weight', 'bold')]}])
+    st.table(df_styled)
 
-        # Define the style function
-        def color_rows(row):
-            color = 'green' if row.Status == 'Completed' else 'red'
-            return [f'color: {color}' for _ in row]
+    # The timeline for your remaining vaccines:
+    st.markdown(
+        "**<span style='color:#708090'>The timeline for your remaining vaccines:</span>**",
+        unsafe_allow_html=True,
+    )
+    vaccines_not_taken = [
+        vaccine for vaccine in eligible_vaccines.keys() if vaccine not in vaccine_selection
+    ]
+    for vaccine in vaccines_not_taken:
+        vaccine_timeline = eligible_vaccines[vaccine]["timeline"]
+        vaccine_timeline_df = pd.DataFrame(vaccine_timeline.items(), columns=["Dose", "Timeline"])
+        st.table(vaccine_timeline_df.style.hide_index().set_properties(**{"text-align": "center"}).\
+                 set_table_styles([{'selector': 'th', 'props': [('text-align', 'center'), ('font-weight', 'bold')]}]))
 
-        # Apply the style
-        df_styled = df.style.hide().set_properties(**{"text-align": "center"}).apply(color_rows, axis=1).\
-            set_table_styles([{'selector': 'th', 'props': [('text-align', 'center'), ('font-weight', 'bold')]}])
+        if vaccine.startswith("Meningococcal:") and meningococcal_note:
+            st.markdown(
+                "<span style='color:#708090'>(Note: You are eligible for multiple types of Meningococcal vaccines. The timeline displayed is specific to the type closest to your age, but you may be eligible for others with different schedules.)</span>",
+                unsafe_allow_html=True,
+            )
 
-        # Display the table
-        st.table(df_styled)
-
-        st.markdown("**The timeline for your remaining vaccines:**")
-        for vaccine in vaccines_not_taken:
-            st.markdown(f"**{vaccine}:**")
-            for dose, time in eligible_vaccines[vaccine]["timeline"].items():
-                st.write(f"{dose}: {time}")
-            if vaccine.startswith("Meningococcal:") and meningococcal_note:
-                st.markdown(
-                    "(Note: You are eligible for multiple types of Meningococcal vaccines. The timeline displayed is specific to the type closest to your age, but you may be eligible for others with different schedules.)"
-                )
-
-        st.sidebar.markdown("**Check vaccine series completion:**")
-        for vaccine in vaccine_selection:
-            vaccine_key = vaccine.strip()
+    # Check vaccine series completion
+    st.sidebar.markdown("**Check vaccine series completion:**", unsafe_allow_html=True)
+    for vaccine in vaccine_selection:
+        vaccine_key = vaccine.strip()
+        show_completion = st.sidebar.radio(
+            f"Do you want to check if you have completed the series for {vaccine_key}?",
+            ["No", "Yes"],
+            index=0,
+        )
+        if show_completion == "Yes":
             doses_taken = st.sidebar.number_input(
                 f"How many doses of {vaccine_key} have you taken?",
                 min_value=0,
@@ -128,8 +135,5 @@ if age > 0:
                     st.sidebar.write(f"You need {doses_needed} more doses of {vaccine_key}.")
                 else:
                     st.sidebar.write(f"You have completed the required doses for {vaccine_key}.")
-
-    else:
-        st.write("You did not select any vaccines. Please try again.")
 else:
-    st.markdown("**You have to enter your age!**")
+    st.write(" ")
