30caff3570b9f2605779e758a623c7f216cc30dd
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-06-19 17:15:16 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+def read_vaccine_info(years):
+    if years < 19:
+        return pd.read_excel("vaccines3.xlsx")
+    else:
+        return pd.read_excel("adultvaccines3.xlsx")
+
 def color_rows(row):
     if row['Status'] == 'Completed':
         color = 'green'
@@ -10,126 +16,56 @@ def color_rows(row):
         color = 'red'
     return ['color: %s' % color]*len(row.values)
 
-def read_vaccine_info(age_year):
-    if age_year >= 19:
-        vaccine_df = pd.read_excel("adultvaccines3.xlsx")
-    else:
-        vaccine_df = pd.read_excel("vaccines3.xlsx")
-    return vaccine_df
-
 months_options = list(range(13))  # 0 to 12
 years_options = list(range(1000))  # 0 to 999
 
-st.title("Vaccine Recommendation Program")
-
-st.markdown(
-    "Welcome to the Vaccine Recommendation Program! This program will tell you which vaccines you are eligible for based on your age. You can also enter which vaccines you have already taken, and the program will tell you if you need any more doses. **Enter the information in the sidebar to get started.**"
-)
-
 st.sidebar.markdown("**Please enter your age:**")
-age_year = st.sidebar.number_input("Years:", min_value=0, max_value=999, value=0, step=1)
 age_month = st.sidebar.selectbox("Months:", months_options)
-
+age_year = st.sidebar.selectbox("Years:", years_options)
 age = (age_month * 30) + (age_year * 365)
 
 if age > 0:
     vaccine_df = read_vaccine_info(age_year)
-    vaccines = {}  # Convert the DataFrame to a dictionary
+
+    # Create the dictionary 'vaccines'
+    vaccines = {}
     for _, row in vaccine_df.iterrows():
         vaccines[row['Vaccine Name']] = {
-            'doses': row['Doses'],
-            'timeline': {f'Dose {i+1}': row[f'Dose {i+1}'] for i in range(row['Doses'])},
+            '# of doses': row['# of doses'],
+            'timeline': {f'Dose {i+1}': row[f'Dose {i+1}'] for i in range(row['# of doses'])},
             'age_range': (row['Start Age'], row['End Age']),
         }
-    
-    st.sidebar.markdown("**Enter the vaccines you have taken (use commas to separate multiple vaccines):**")
-    vaccine_input = st.sidebar.text_input("", value="", max_chars=None, key=None, type='default')
-    vaccine_input = vaccine_input.lower().split(',')
-    vaccine_selection = [name.strip().capitalize() for name in vaccine_input if name.strip() != '']
 
-    # Vaccine eligibility table
-    df = pd.DataFrame(columns=["Vaccine Name", "Status"])
-    df["Vaccine Name"] = list(vaccines.keys())
-    df["Status"] = "Pending"
+    eligible_vaccines = {k: v for k, v in vaccines.items() if v['age_range'][0]*365 <= age <= v['age_range'][1]*365}
+    vaccine_selection = st.sidebar.multiselect("Select the vaccines you have already taken:", options=list(eligible_vaccines.keys()))
 
-    for vaccine in vaccine_selection:
-        if vaccine in df["Vaccine Name"].values:
-            df.loc[df['Vaccine Name'] == vaccine, 'Status'] = 'Completed'
-    
-    st.sidebar.markdown("**Check vaccine series completion:**")
-    for vaccine in vaccine_selection:
-        vaccine_key = vaccine.strip()
-
-        if vaccine_key == 'Influenza' and age_year >= 19:
-            influenza_done = st.sidebar.radio(
-                f"Have you completed your annual influenza vaccine this year?",
-                ["No", "Yes"],
-                index=0,
-            )
-            if influenza_done == 'Yes':
-                st.sidebar.write(f"You have completed the required doses for {vaccine_key}.")
-                df.loc[df['Vaccine Name'] == vaccine_key, 'Status'] = 'Completed'
-            else:
-                st.sidebar.write(f"You need 1 more dose of {vaccine_key} to complete the series.")
-                df.loc[df['Vaccine Name'] == vaccine_key, 'Status'] = 'In Progress'
-        elif vaccine_key == 'Tdap' and age_year >= 19:
-            tdap_done = st.sidebar.radio(
-                f"Have you received the Tdap vaccine in the past 10 years?",
-                ["No", "Yes"],
-                index=0,
-            )
-            if tdap_done == 'Yes':
-                st.sidebar.write(f"You have completed the required doses for {vaccine_key}.")
-                df.loc[df['Vaccine Name'] == vaccine_key, 'Status'] = 'Completed'
-            else:
-                st.sidebar.write(f"You need 1 more dose of {vaccine_key} to complete the series.")
-                df.loc[df['Vaccine Name'] == vaccine_key, 'Status'] = 'In Progress'
-        elif vaccine_key in vaccines:
-            show_completion = st.sidebar.radio(
-                f"Do you want to check if you have completed the series for {vaccine_key}?",
-                ["No", "Yes"],
-                index=0,
-            )
-            if show_completion == "Yes":
-                doses_taken = st.sidebar.number_input(
-                    f"How many doses of {vaccine_key} have you taken?",
-                    min_value=1,
-                    value=1,
-                )
-                if doses_taken > 0:
-                    doses_needed = vaccines[vaccine_key]["doses"] - doses_taken  # Use 'vaccines' instead of 'eligible_vaccines'
-                    if doses_needed > 0:
-                        st.sidebar.write(f"You need {doses_needed} more doses of {vaccine_key}.")
-                        df.loc[df['Vaccine Name'] == vaccine_key, 'Status'] = 'In Progress'
-                    else:
-                        st.sidebar.write(f"You have completed the required doses for {vaccine_key}.")
-                        df.loc[df['Vaccine Name'] == vaccine_key, 'Status'] = 'Completed'
+    if vaccine_selection:
+        st.sidebar.markdown("**Check vaccine series completion:**", unsafe_allow_html=True)
+        for vaccine in vaccine_selection:
+            if vaccine == "Influenza":
+                flu_shot = st.sidebar.radio("Have you completed your annual influenza vaccine this year?", ["No", "Yes"])
+                if flu_shot == "Yes":
+                    st.sidebar.write("You have completed your series for Influenza.")
                 else:
-                    df.loc[df['Vaccine Name'] == vaccine_key, 'Status'] = 'Pending'
-    
-    if vaccines_not_taken:
-        st.markdown(
-            "**<span style='color:#708090'>The timeline for your remaining vaccines:</span>**",
-            unsafe_allow_html=True,
-        )
-
-    st.table(df.style.apply(color_rows, axis=1).set_properties(**{'text-align': 'center'}))
-    
-    hide_table_row_index = """
-            <style>
-            thead tr th:first-child {display:none}
-            tbody th {display:none}
-            </style>
-            """
-    # Inject CSS with Markdown
-    st.markdown(hide_table_row_index, unsafe_allow_html=True)
-    
-    for vaccine in vaccines_not_taken:
-        st.markdown(
-            f"**<span style='color:#708090'>{vaccine}:</span>**", unsafe_allow_html=True
-        )
-        timeline_data = []
-        for dose, time in eligible_vaccines[vaccine]["timeline"].items():
-            timeline_data.append([dose, time])
-        timeline_df = pd.DataFrame(timeline_data, columns=["Dose", "Time"])
-        st.table(timeline_df)
+                    st.sidebar.write("You need to take one dose of Influenza vaccine to complete the series.")
+            elif vaccine == "Tdap":
+                tdap_shot = st.sidebar.radio("Have you received the Tdap vaccine in the past 10 years?", ["No", "Yes"])
+                if tdap_shot == "Yes":
+                    st.sidebar.write("You have completed your series for Tdap.")
+                else:
+                    st.sidebar.write("You need to take one dose of Tdap vaccine to complete the series for the next 10 years.")
+            else:
+                show_completion = st.sidebar.radio(f"Do you want to check if you have completed the series for {vaccine}?", ["No", "Yes"])
+                if show_completion == "Yes":
+                    doses_taken = st.sidebar.number_input(f"How many doses of {vaccine} have you taken?", min_value=1, value=1)
+                    if doses_taken > 0:
+                        doses_needed = vaccines[vaccine]['# of doses'] - doses_taken
+                        if doses_needed > 0:
+                            st.sidebar.write(f"You need to take {doses_needed} more dose(s) of {vaccine} to complete the series.")
+                        else:
+                            st.sidebar.write(f"You have completed your series for {vaccine}.")
+        # Add table
+        eligible_vaccines_df = pd.DataFrame(eligible_vaccines).transpose()
+        eligible_vaccines_df.drop(['timeline', 'age_range'], axis=1, inplace=True)
+        eligible_vaccines_df['Status'] = eligible_vaccines_df.apply(lambda row: 'Completed' if row.name in vaccine_selection else 'Pending', axis=1)
+        st.dataframe(eligible_vaccines_df.style.apply(color_rows, axis=1))
