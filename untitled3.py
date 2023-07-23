fd52628ba2174ff245b2770e5845d1f09bc7ebee
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-07-22 20:37:26 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -150,7 +150,10 @@ if age > 0:
             "**<span style='color:#708090'>The timeline for your remaining vaccines:</span>**",
             unsafe_allow_html=True,
         )
-        for vaccine in vaccines_not_taken and 'Status' = 'In Progress':
+        for vaccine in vaccines_not_taken 
+        and
+        for vaccine, info in eligible_vaccines.items():
+        status = "Pending":
             st.markdown(
                 f"**<span style='color:#708090'>{vaccine}:</span>**", unsafe_allow_html=True
             )
