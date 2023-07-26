f01de23372da5e7cef72985ce8f56881bda5cff1
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-07-26 16:07:00 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -36,7 +36,6 @@ else:
 # Read the vaccine information from the Excel file
 vaccine_df = pd.read_excel("vaccinesfull.xlsx", sheet_name=sheet)
 
-
 # Convert the DataFrame to a dictionary
 vaccines = {}
 for _, row in vaccine_df.iterrows():
@@ -53,7 +52,7 @@ for _, row in vaccine_df.iterrows():
             dose_max = row[f"Dose {i} Max"]
             doses_info[f"Dose {i}"] = {"min": dose_min, "max": dose_max}
             timeline[f"Dose {i}"] = row[f"Dose {i}"]
-    vaccines[vaccine] = {"ages": age_range, "doses": doses, "doses_info": doses_info, "timeline": timeline, "condition 1": condition_1, "condition 2": condition_2} # Added condition 1 and condition 2 to the dictionary
+    vaccines[vaccine] = {"ages": age_range, "doses": doses, "doses_info": doses_info, "timeline": timeline, "condition 1": condition_1, "condition 2": condition_2}  # Added condition 1 and condition 2 to the dictionary
 
 # Initialize vaccine_selection as an empty list
 vaccine_selection = []
@@ -61,9 +60,7 @@ vaccine_selection = []
 if age > 0:
     # Determine which vaccines the user is eligible for
     eligible_vaccines = {k: v for k, v in vaccines.items() if age in v["ages"]}
-
-    # (Remaining part of the code...)
-
+    
     # Sidebar for already taken vaccines
     st.sidebar.markdown(
         "**<span style='color:black'>Please select the vaccines you have already taken (You can select multiple):</span>**",
@@ -72,45 +69,57 @@ if age > 0:
     vaccine_selection = st.sidebar.multiselect(
         "", list(eligible_vaccines.keys()) + ["None"]
     )
+    
+    # Define the data for the table
+    data = []
+    for vaccine, info in eligible_vaccines.items():
+        status = "Completed" if vaccine in vaccine_selection else "Pending"
+        data.append([vaccine, info["doses"], status])
 
-    # (Remaining part of the code...)
+    # Create the DataFrame
+    df = pd.DataFrame(data, columns=["Vaccine Name", "Total Doses", "Status"])
+    df = df.sort_values(by="Status", ascending=False)
+    df = df.reset_index(drop=True)
 
-vaccines_not_taken = [
-    vaccine for vaccine in eligible_vaccines.keys() if vaccine not in vaccine_selection or df[df['Vaccine Name'] == vaccine]['Status'].values[0] == 'In Progress'
-]
+    # Only show the vaccine series completion check if 'None' was not selected
+    if "None" not in vaccine_selection:
+        st.sidebar.markdown("**Check vaccine series completion:**", unsafe_allow_html=True)
+        for vaccine in vaccine_selection:
+            vaccine_key = vaccine.strip()
+            show_completion = st.sidebar.radio(
+                f"Do you want to check if you have completed the series for {vaccine_key}?",
+                ["No", "Yes"],
+                index=0,
+            )
+            if show_completion == "Yes":
+                doses_taken = st.sidebar.number_input(
+                    f"How many doses of {vaccine_key} have you taken?",
+                    min_value=1,
+                    value=1,
+                )
+                if doses_taken > 0:
+                    doses_needed = vaccines[vaccine_key]["doses"] - doses_taken  # Use 'vaccines' instead of 'eligible_vaccines'
+                    if doses_needed > 0:
+                        st.sidebar.write(f"You need {doses_needed} more doses of {vaccine_key}.")
+                        df.loc[df['Vaccine Name'] == vaccine_key, 'Status'] = 'In Progress'
+                    else:
+                        st.sidebar.write(f"You have completed the required doses for {vaccine_key}.")
+                else:
+                    df.loc[df['Vaccine Name'] == vaccine_key, 'Status'] = 'Pending'
 
-# (Remaining part of the code...)
-
+    vaccines_not_taken = [
+        vaccine for vaccine in eligible_vaccines.keys() if vaccine not in vaccine_selection or df[df['Vaccine Name'] == vaccine]['Status'].values[0] == 'In Progress'
+    ]
 
     st.table(df.style.apply(color_rows, axis=1).set_properties(**{'text-align': 'center'}))
 
-    hide_table_row_index = """
-            <style>
-            thead tr th:first-child {display:none}
-            tbody th {display:none}
-            </style>
-            """
-    # Inject CSS with Markdown
-    st.markdown(hide_table_row_index, unsafe_allow_html=True)
-
     if len(vaccines_not_taken) > 0:
         st.markdown(
-            "**<span style='color:#708090'>The timeline for your remaining vaccines:</span>**",
+            "**<span style='color:#708090'>The timeline below shows when you should take each dose for the vaccines you have not taken yet:</span>**",
             unsafe_allow_html=True,
         )
         for vaccine in vaccines_not_taken:
-            st.markdown(
-                f"**<span style='color:#708090'>{vaccine}:</span>**", unsafe_allow_html=True
-            )
-            timeline_data = []
-            for dose, time in eligible_vaccines[vaccine]["timeline"].items():
-                timeline_data.append([dose, time])
-            timeline_df = pd.DataFrame(timeline_data, columns=["Dose", "Time"])
-            st.table(timeline_df)
-
-            # Print conditions if they exist and the status is not 'Completed'
-            vaccine_dict = vaccines[vaccine]
-            if vaccine_dict["condition 1"]:
-                st.write("Condition 1: " + vaccine_dict["condition 1"])
-            if vaccine_dict["condition 2"]:
-                st.write("Condition 2: " + vaccine_dict["condition 2"])
+            vaccine_info = vaccines[vaccine]  # Use 'vaccines' instead of 'eligible_vaccines'
+            st.write(f"**{vaccine}**")
+            for dose, time in vaccine_info["timeline"].items():
+                st.write(f"{dose}: {time} days")
