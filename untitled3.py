fcb2feb575f20e09d5f3efd99e75ed1bcc7b1b2f
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-07-26 23:43:57 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -89,9 +89,8 @@ if age > 0:
     eligibility_criteria_check = st.sidebar.checkbox("Eligibility and Ineligibility Criteria")
     conditions_dosing_check = st.sidebar.checkbox("Conditions and Alternative dosing")
 
-    # Display the table if the Normal Vaccine schedule checkbox is checked
-    if normal_schedule_check:
-        st.table(df.style.apply(color_rows, axis=1).set_properties(**{'text-align': 'center'}))
+    # Always Display the first table regardless of the checkbox state
+    st.table(df.style.apply(color_rows, axis=1).set_properties(**{'text-align': 'center'}))
 
     hide_table_row_index = """
             <style>
@@ -132,30 +131,30 @@ if age > 0:
     ]
 
     if len(vaccines_not_taken) > 0:
-        st.markdown(
-            "**<span style='color:#708090'>The timeline for your remaining vaccines:</span>**",
-            unsafe_allow_html=True,
-        )
+        if normal_schedule_check:
+            st.markdown(
+                "**<span style='color:#708090'>The timeline for your remaining vaccines:</span>**",
+                unsafe_allow_html=True,
+            )
         for vaccine in vaccines_not_taken:
             st.markdown(
                 f"**<span style='color:#708090'>{vaccine}:</span>**", unsafe_allow_html=True
             )
-            # Display the Eligibility and Ineligibility info if the corresponding checkbox is checked
+            # Display the eligibility and ineligibility info if the corresponding checkbox is checked
             if eligibility_criteria_check:
-                if eligible_vaccines[vaccine]["eligibility"]:
-                    st.markdown(f"**<span style='color:green'>**You are eligible for this vaccine if:</span>** {eligible_vaccines[vaccine]['eligibility']}", unsafe_allow_html=True)
+                st.markdown(f"**<span style='color:green'>You are eligible for this vaccine if:</span>** {eligible_vaccines[vaccine]['eligibility']}", unsafe_allow_html=True)
                 if eligible_vaccines[vaccine]["ineligibility"]:
-                    st.markdown(f"**<span style='color:red'>**You are not eligible for this vaccine if:</span>** {eligible_vaccines[vaccine]['ineligibility']}", unsafe_allow_html=True)
-            st.table(pd.DataFrame(eligible_vaccines[vaccine]["timeline"], index=["Timeline"]))
-
+                    st.markdown(f"**<span style='color:red'>You are not eligible for this vaccine if:</span>** {eligible_vaccines[vaccine]['ineligibility']}", unsafe_allow_html=True)
+            if normal_schedule_check:
+                st.table(pd.DataFrame(eligible_vaccines[vaccine]["timeline"], index=["Timeline"]))
             condition_dosing_data = []
             for condition, dosing in eligible_vaccines[vaccine]["condition_dosing"].items():
                 condition_dosing_data.append([condition, dosing])
-
             # Display the conditions and alternate dosing table if the corresponding checkbox is checked
             if conditions_dosing_check and len(condition_dosing_data) > 0:
                 st.markdown(
-                    f"**<span style='color:#708090'>Conditions and Alternate Dosing for {vaccine}:</span>**", unsafe_allow_html=True
+                    f"**<span style='color:#708090'>Conditions and Alternate Dosing for {vaccine}:</span>**",
+                    unsafe_allow_html=True
                 )
                 condition_dosing_df = pd.DataFrame(condition_dosing_data, columns=["Condition", "Alternate Dosing"])
                 st.table(condition_dosing_df)
