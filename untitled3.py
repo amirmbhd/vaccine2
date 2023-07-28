4a59a7112bbbfeb23a528fe28cf4f9332725dfc6
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-07-28 12:50:08 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -144,7 +144,7 @@ if age > 0:
           
 
           
-            # ...
+
             if eligibility_criteria_check:
                 for i, vaccine in enumerate(vaccines_not_taken):
                     if eligible_vaccines[vaccine]['eligibility'] or eligible_vaccines[vaccine]['ineligibility']:
@@ -155,15 +155,15 @@ if age > 0:
                             st.markdown(f"**<span style='color:green'>You are eligible for this vaccine if:</span>** {eligible_vaccines[vaccine]['eligibility']}", unsafe_allow_html=True)
                         if eligible_vaccines[vaccine]["ineligibility"]:
                             st.markdown(f"**<span style='color:red'>You are not eligible for this vaccine if:</span>** {eligible_vaccines[vaccine]['ineligibility']}", unsafe_allow_html=True)
-                    
+                        
                         # Ask the user for their eligibility status after displaying the eligibility and ineligibility criteria
-                        # Include the index `i` in the key to ensure uniqueness
-                        eligibility_status = st.radio(f"Based on the information provided, what is your eligibility status for {vaccine}?", ['Eligible', 'Ineligible'], key=f"{vaccine}_eligibility_{i}")
+                        # Generate a unique key for each radio button by using both the vaccine name and the index
+                        unique_key = f"{vaccine}_eligibility_{i}"
+                        eligibility_status = st.radio(f"Based on the information provided, what is your eligibility status for {vaccine}?", ['Eligible', 'Ineligible'], key=unique_key)
                         if eligibility_status == 'Eligible':
                             st.write(f"You have selected that you are eligible for {vaccine}.")
                         elif eligibility_status == 'Ineligible':
                             st.write(f"You have selected that you are not eligible for {vaccine}.")
-            # ...
 
 
             if normal_schedule_check:
