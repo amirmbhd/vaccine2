a0e83f9b405db0d8d8eebe81c0ecb3f4e30794fc
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-07-28 12:27:05 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -141,19 +141,25 @@ if age > 0:
             st.markdown(
                 f"**<span style='color:#5C27E7'>{vaccine}:</span>**", unsafe_allow_html=True
             )
-            # Display the eligibility and ineligibility info if the corresponding checkbox is checked and data exists
+          
+                        # ...
             if eligibility_criteria_check:
-                if eligible_vaccines[vaccine]['eligibility']:
-                    st.markdown(f"**<span style='color:green'>You are eligible for this vaccine if:</span>** {eligible_vaccines[vaccine]['eligibility']}", unsafe_allow_html=True)
-                if eligible_vaccines[vaccine]["ineligibility"]:
-                    st.markdown(f"**<span style='color:red'>You are not eligible for this vaccine if:</span>** {eligible_vaccines[vaccine]['ineligibility']}", unsafe_allow_html=True)
+                for vaccine in vaccines_not_taken:
+                    if eligible_vaccines[vaccine]['eligibility'] or eligible_vaccines[vaccine]['ineligibility']:
+                        if eligible_vaccines[vaccine]['eligibility']:
+                            st.markdown(f"**<span style='color:green'>You are eligible for this vaccine if:</span>** {eligible_vaccines[vaccine]['eligibility']}", unsafe_allow_html=True)
+                        if eligible_vaccines[vaccine]["ineligibility"]:
+                            st.markdown(f"**<span style='color:red'>You are not eligible for this vaccine if:</span>** {eligible_vaccines[vaccine]['ineligibility']}", unsafe_allow_html=True)
+            
+                        # Ask the user for their eligibility status after displaying the eligibility and ineligibility criteria
+                        eligibility_status = st.radio(f"Based on the information provided, what is your eligibility status for {vaccine}?", ['Eligible', 'Ineligible'])
+                        if eligibility_status == 'Eligible':
+                            st.write(f"You have selected that you are eligible for {vaccine}.")
+                        elif eligibility_status == 'Ineligible':
+                            st.write(f"You have selected that you are not eligible for {vaccine}.")
+            # ...
 
-                # Ask the user for their eligibility status after displaying the eligibility and ineligibility criteria
-                if eligibility_status == 'Eligible':
-                    st.write(f"You have selected that you are eligible for {vaccine}.")
-                elif eligibility_status == 'Ineligible':
-                    st.write(f"You have selected that you are not eligible for {vaccine}.")
-    
+
             if normal_schedule_check:
                 st.table(pd.DataFrame(eligible_vaccines[vaccine]["timeline"], index=["Timeline"]))
             condition_dosing_data = []
