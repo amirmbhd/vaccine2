15ed2cbe46eeb878926f29fb2a1ad85bc1b7081b
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-08-06 10:55:43 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -171,6 +171,9 @@ if age > 0:
                 unsafe_allow_html=True,
             )
         for vaccine in vaccines_not_taken:
+            st.markdown(
+                f"**<span style='color:#5C27E7'>{vaccine}:</span>**", unsafe_allow_html=True
+            )
             # Display the eligibility and ineligibility info if the corresponding checkbox is checked and data exists
             if eligibility_criteria_check:
                 if eligible_vaccines[vaccine]['eligibility']:
@@ -178,9 +181,6 @@ if age > 0:
                 if eligible_vaccines[vaccine]["ineligibility"]:
                     st.markdown(f"**<span style='color:red'>You are not eligible for this vaccine if meeting any of these conditions/criteria:</span>** {eligible_vaccines[vaccine]['ineligibility']}", unsafe_allow_html=True)
             if normal_schedule_check:
-                st.markdown(
-                f"**<span style='color:#5C27E7'>{eligible_vaccines}:</span>**", unsafe_allow_html=True
-            )
                 st.table(pd.DataFrame(eligible_vaccines[vaccine]["timeline"], index=["Timeline"]))
             condition_dosing_data = []
             for condition, dosing in eligible_vaccines[vaccine]["condition_dosing"].items():
