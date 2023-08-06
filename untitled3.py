e78735bf1656801be30f86b8c4b886462c900b82
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-08-06 12:09:21 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -120,7 +120,12 @@ if age > 0:
         # split the dataframe into two based on the 'Schedule' column
     df_conditional = df[df['Schedule'] == 'Conditional']
     df_non_conditional = df.drop(df_conditional.index)
-        
+
+    # Add 'Schedule' to the DataFrame columns
+    df = pd.DataFrame(data, columns=["Vaccine Name", "Total Doses", "Status", "Schedule"])
+    df = df.sort_values(by="Status", ascending=False)
+    df = df.reset_index(drop=True)
+   
         # At the end of the sidebar, ask the user to review eligibility criteria 
     st.sidebar.markdown("**Please review eligibility criteria and select your eligibility status for the following vaccines:**")
         # For each vaccine with a 'Conditional' status, present radio buttons
@@ -141,11 +146,6 @@ if age > 0:
 
     
                     
-
-    # Add 'Schedule' to the DataFrame columns
-    df = pd.DataFrame(data, columns=["Vaccine Name", "Total Doses", "Status", "Schedule"])
-    df = df.sort_values(by="Status", ascending=False)
-    df = df.reset_index(drop=True)
 
 
     
