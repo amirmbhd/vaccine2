4a8baa6768331f934978cbdf6615d936cb043e3f
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-08-06 14:38:37 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -90,6 +90,14 @@ if age > 0:
     # Determine which vaccines the user is eligible for
     eligible_vaccines = {k: v for k, v in vaccines.items() if age in v["ages"]}
 
+    # Sidebar for already taken vaccines
+    st.sidebar.markdown(
+        "**<span style='color:black'>Please select the vaccines you have already taken (You can select multiple):</span>**",
+        unsafe_allow_html=True,
+    )
+    vaccine_selection = st.sidebar.multiselect(
+        "", list(eligible_vaccines.keys()) + ["None"]
+    )
         
     # Define the checkboxes in the sidebar
     normal_schedule_check = st.sidebar.checkbox("Normal Vaccine schedule")
@@ -102,18 +110,11 @@ if age > 0:
         checkbox_label = "Conditions and Alternate Dosing"
     
     conditions_dosing_check = st.sidebar.checkbox(checkbox_label)
-
     
-    # Sidebar for already taken vaccines
-    st.sidebar.markdown(
-        "**<span style='color:black'>Please select the vaccines you have already taken (You can select multiple):</span>**",
-        unsafe_allow_html=True,
-    )
-    vaccine_selection = st.sidebar.multiselect(
-        "", list(eligible_vaccines.keys()) + ["None"]
-    )
 
-
+    # At the end of the sidebar, ask the user to review eligibility criteria 
+    st.sidebar.markdown("**Please review eligibility criteria and select your eligibility status for the following vaccines:**")
+    
     # Prompt for radio buttons before constructing the DataFrame
     eligibility_statuses = {}
     for vaccine, info in eligible_vaccines.items():
@@ -171,9 +172,7 @@ if age > 0:
     df_conditional = df[df['Schedule'] == 'Conditional']
     df_non_conditional = df.drop(df_conditional.index)
 
-        # At the end of the sidebar, ask the user to review eligibility criteria 
-    st.sidebar.markdown("**Please review eligibility criteria and select your eligibility status for the following vaccines:**")
-    
+
  
     for vaccine, status in eligibility_statuses.items():
         if status == "Under Review":
