460a5bedf46e3ebe9df0f0b13b515cb145a4ffb2
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-08-05 22:49:29 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -189,7 +189,7 @@ if age > 0:
                 
                 if eligibility_info_present:
                     user_choice = st.radio(
-                        f"**Based on the information above, select your eligibility for the {vaccine} vaccine:**",
+                        f"Based on the information above, select your eligibility for the {vaccine} vaccine:",
                         ('Ineligible', 'Eligible'),
                         key=f"eligibility_radio_{vaccine}"  # This makes the key unique for each vaccine
                     )
