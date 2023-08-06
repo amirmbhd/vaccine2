3a071b894664871f69976d2f902a1576c0304589
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-08-06 10:59:24 -0400

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -170,10 +170,10 @@ if age > 0:
                 "**<span style='color:#254912'>The timeline for your remaining vaccines:</span>**",
                 unsafe_allow_html=True,
             )
-        for vaccine in vaccines_not_taken:
-            st.markdown(
-                f"**<span style='color:#5C27E7'>The normal timeline for{vaccine}:</span>**", unsafe_allow_html=True
-            )
+            for vaccine in vaccines_not_taken:
+                st.markdown(
+                    f"**<span style='color:#5C27E7'>The normal timeline for {vaccine}:</span>**", unsafe_allow_html=True
+                )
             # Display the eligibility and ineligibility info if the corresponding checkbox is checked and data exists
             if eligibility_criteria_check:
                 if eligible_vaccines[vaccine]['eligibility']:
